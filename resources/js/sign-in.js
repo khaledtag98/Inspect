bc68257dfc8eb9769a3cdbb354cd71e6b1c536b6
Shaimaa98 <40139825+Shaimaa98@users.noreply.github.com>
2020-11-29 18:29:36 +0200

    Modified sign-in page and added registration form

---
+++ resources/js/sign-in.js
@@ -0,0 +1,21 @@
+$("document").ready(function () {
+    $(".tab-slider--body").hide();
+    $(".tab-slider--body:first").show();
+});
+let currntTab = "tab1";
+$(".tab-slider--nav li").click(function () {
+    let activeTab = $(this).attr("rel");
+    if (currntTab != activeTab ) {
+        $(".tab-slider--body").hide();
+        console.log(currntTab + " = " + activeTab);
+    }
+     $("#" + activeTab).fadeIn();
+    if (activeTab == "tab2") {
+        $('.tab-slider--tabs').addClass('slide');
+    } else {
+        $('.tab-slider--tabs').removeClass('slide');
+    }
+    $(".tab-slider--nav li").removeClass("active");
+    $(this).addClass("active");
+    currntTab = activeTab;
+});
--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -11,20 +11,51 @@
     <div class='flex my-40'>
         <div class="w-1/2 p-8 ">
             <div class='flex'>
-                <button class='w-1/3 px-6 py-4 m-auto mr-0 border-2 border-r-2 border-gray-700 bg-primary-color'>Sign In</button>
-                <button class='w-1/3 px-6 py-4 m-auto ml-0 border-2 border-l-0 border-gray-700'>Register</button>
             </div>
-            <form class="block my-6">
-                <label>Email </label>
-                <input class="w-full px-2 py-1 mt-2 mb-8 border-2 border-gray-600" type="email" name="" placeholder="" aria-describedby="helpId">
-                <label>Password </label>
-                <input class="w-full px-2 py-1 mt-2 border-2 border-gray-600" type="password" name="" placeholder="" aria-describedby="helpId">
-                <div>
-                    <a href=""
-                        class="flex items-center justify-center w-1/2 px-8 py-3 m-auto mt-12 text-base font-medium leading-6 text-white transition duration-150 ease-in-out border border-transparent shadow bg-button1 btn hover-no-underline focus:outline-none focus:shadow-outline-indigo md:py-4 md:text-lg md:px-10">
-                        Sign In</a>
+            <div class="container">
+                <div class="tab-slider--nav">
+                    <ul class="tab-slider--tabs">
+                        <li class="tab-slider--trigger active" rel="tab1">Sign In</li>
+                        <li class="tab-slider--trigger" rel="tab2">Register</li>
+                    </ul>
                 </div>
-            </form>
+                <div class="tab-slider--container">
+                    <div id="tab1" class="tab-slider--body">
+                        <form class="block my-8">
+                            <label>Email </label>
+                            <input class="w-full px-2 py-1 mt-2 mb-4 border-2 border-gray-600" type="email" name="" placeholder="" aria-describedby="helpId">
+                            <label>Password </label>
+                            <input class="w-full px-2 py-1 mt-2 border-2 border-gray-600" type="password" name="" placeholder="" aria-describedby="helpId">
+                            <div>
+                                <a href=""
+                                    class="flex items-center justify-center w-1/2 px-8 py-3 m-auto mt-12 text-base font-medium leading-6 text-white transition duration-150 ease-in-out border border-transparent shadow bg-button1 btn hover-no-underline focus:outline-none focus:shadow-outline-indigo md:py-4 md:text-lg md:px-10">
+                                    Sign In</a>
+                            </div>
+                        </form>
+                    </div>
+                    <div id="tab2" class="tab-slider--body">
+                        <form class="block my-8">
+                            <label>Full Name </label>
+                            <div class='flex'>
+                                <input class='w-3/4 px-1 px-2 py-1 mt-2 mb-4 mr-4 border-2 border-gray-600' type="text" placeholder="First Name">
+                                <input class='w-3/4 px-1 px-2 py-1 mt-2 mb-4 border-2 border-gray-600' type="text" placeholder="Last Name">
+                            </div>
+                            <label>Email</label>
+                            <input class="w-full px-2 py-1 mt-1 mb-4 border-2 border-gray-600" type="email" name="" placeholder="" aria-describedby="helpId">
+                            <label>Password </label>
+                            <input class="w-full px-2 py-1 mt-1 mb-4 border-2 border-gray-600" type="password" name="" placeholder="" aria-describedby="helpId">
+                            <label>Confirm Password </label>
+                            <input class="w-full px-2 py-1 mt-1 border-2 border-gray-600" type="password" name="" placeholder="" aria-describedby="helpId">
+                            <div>
+                                <a href=""
+                                    class="flex items-center justify-center w-1/2 px-8 py-3 m-auto mt-12 text-base font-medium leading-6 text-white transition duration-150 ease-in-out border border-transparent shadow bg-button1 btn hover-no-underline focus:outline-none focus:shadow-outline-indigo md:py-4 md:text-lg md:px-10">
+                                    Register</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
         </div>
         <div class="w-1/2 ml-10">
             <img src="http://localhost/our-beautiful-project/resources/img/undraw_authentication_fsn5.svg" alt="">
@@ -35,4 +66,8 @@
 @endsection
 
 @section('customJS')
+<script src="http://localhost/our-beautiful-project/resources/js/sign-in.js"></script>
+
+
+
 @endsection
