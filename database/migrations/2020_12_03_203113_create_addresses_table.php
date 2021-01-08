238eee95b451418b901623d7b1fbe982b78350f6
Khaled Ahmed Tag <55442919+khaledtag98@users.noreply.github.com>
2021-01-08 22:14:11 +0200

    updates

--- database/migrations/2020_12_03_203113_create_addresses_table.php
+++ database/migrations/2020_12_03_203113_create_addresses_table.php
@@ -15,14 +15,12 @@ class CreateAddressesTable extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-
-            $table->string('country');
-            $table->string('city');
-            $table->string('street');
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('street')->nullable();
             $table->timestamps();
             $table->bigInteger('company_id')->unsigned();
-            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
-
+//            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
         });
     }
 
